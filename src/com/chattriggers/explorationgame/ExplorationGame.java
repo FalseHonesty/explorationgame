f93fd19bdffc5acb8742df47e13efecbd3c3c94d
FalseHonesty <skipboman0@gmail.com>
2017-11-14 20:58:40 -0500

    Changed to processing

---
+++ src/com/chattriggers/explorationgame/ExplorationGame.java
@@ -0,0 +1,35 @@
+package com.chattriggers.explorationgame;
+
+import lombok.Getter;
+import processing.core.PApplet;
+
+/**
+ * Copyright 2017 (c) FalseHonesty
+ */
+
+public class ExplorationGame extends PApplet {
+    @Getter
+    private static ExplorationGame explorationGame;
+
+    @Override
+    public void settings() {
+        size(500, 500);
+    }
+
+    @Override
+    public void draw() {
+        ellipse(mouseX, mouseY, 50, 50);
+    }
+
+    @Override
+    public void mousePressed() {
+        background(64);
+    }
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"ExplorationGame"};
+
+        explorationGame = new ExplorationGame();
+        PApplet.runSketch(processingArgs, explorationGame);
+    }
+}
